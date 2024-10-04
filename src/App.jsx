b179e440a324376d3bb3c061520dd92851180a17
HAZIK BHAT <bhatkhalid3171@gmail.com>
2024-10-04 11:01:17 +0530

    client changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,27 @@ import AppRoutes from "./Routes/AppRoutes";
 import ResetPassword from "./Pages/ResetPassword/ResetPassword";
 import PasswordRecovery from "./Pages/PasswordRecovery/PasswordRecovery";
 import { useEffect, useState } from "react";
-// import { BASE_URI } from "./Config/url";
-// import useFetch from "./hooks/useFetch";
-// import axios from "axios";
-// import { userCartActions } from "./Store/cartSlice";
-// import { useDispatch } from "react-redux";
-// import { payoutActions } from "./Store/payoutSlice";
+import { BASE_URI } from "./Config/url";
+import useFetch from "./hooks/useFetch";
+import axios from "axios";
+import { userCartActions } from "./Store/cartSlice";
+import { useDispatch } from "react-redux";
+import { payoutActions } from "./Store/payoutSlice";
 
 const App = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [search, setSearch] = useState("");
   // const [cartItemNumber, setCartItemNumber] = useState(0);
 
   // const url = `${BASE_URI}/api/v1/cart`;
-  // const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
   // const { data } = useFetch(url, {
   //   headers: {
   //     Authorization: "Bearer " + token,
   //   },
   // });
 
-  // useEffect(() => {
+  useEffect(() => {
   //   axios({
   //     method: "GET",
   //     url,
@@ -40,20 +40,20 @@ const App = () => {
   //     () => {}
   //   );
 
-  //   axios
-  //     .get(`${BASE_URI}/api/v1/admin/payoutRequest`, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     })
-  //     .then((res) => {
-  //       dispatch(payoutActions.setNotifications(res?.data?.data.length));
-  //     });
+    axios
+      .get(`${BASE_URI}/api/v1/admin/payoutRequest`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        dispatch(payoutActions.setNotifications(res?.data?.data.length));
+      });
 
-  //   if (data) {
-  //     setCartItemNumber(data?.cart?.length);
-  //   }
-  // }, []);
+    // if (data) {
+    //   setCartItemNumber(data?.cart?.length);
+    // }
+  }, []);
 
   return (
     <BrowserRouter>
